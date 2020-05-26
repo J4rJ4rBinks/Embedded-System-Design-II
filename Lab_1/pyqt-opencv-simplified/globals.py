340364eb81d3dfcfc099e005a218e08819c0486b
J4rJ4rBinks <50147212+J4rJ4rBinks@users.noreply.github.com>
2020-05-26 09:22:05 -0500

    Add files via upload

---
+++ Lab_1/pyqt-opencv-simplified/globals.py
@@ -0,0 +1,17 @@
+import os
+import mmap
+import struct
+
+
+class globals():
+    rMin = 4
+    rMax = 48
+    gMin = 255
+    gMax = 0
+    bMin = 0
+    bMax = 255
+    Brightness = 2
+    contrast = 25
+    image_filepath = "../images/loadfile.jpg"
+    filterFile = ""
+    filterOption = 0
